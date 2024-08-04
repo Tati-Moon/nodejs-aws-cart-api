fe1661f2dcdded14354d7983a31e404b5fd6c6d1
Tati <tati.moon89@gmail.com>
2024-08-05 00:46:34 +0200

    Update main

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,18 @@ import helmet from 'helmet';
 
 import { AppModule } from './app.module';
 
-
-
 const port = process.env.PORT || 4000;
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    
-    app.enableCors({ origin: (req, callback) => callback(null, true), });
-    app.use(helmet());
-    
-    await app.listen(port, '0.0.0.0');
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: (req, callback) => callback(null, true),
+  });
+  app.use(helmet());
+
+  await app.listen(port);
 }
 bootstrap().then(() => {
-    console.log('App is running on %s port', port);
+  console.log('App is running on %s port', port);
 });
